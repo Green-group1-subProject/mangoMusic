55f2b0c91524c4fbcb57c6f14ae6a6fabb38b3da
Parkys <Parkys@192.168.219.100>
2021-11-25 18:07:56 +0900

    haha영선

--- src/main/java/com/mangoMusic/mainPage/Test3.java
+++ src/main/java/com/mangoMusic/mainPage/Test3.java
@@ -1,10 +1,9 @@
 package com.mangoMusic.mainPage;
 
-public class Test2 {
+public class Test3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hahahaahahah");
 	}
 
 }
