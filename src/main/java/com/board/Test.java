3f1a1921f3bfa89bc5871fa3cc55c420c0dc223b
DonghanKang <kdonghan2006@naver.com>
2021-11-29 17:44:23 +0900

    Board Team commit Test

--- src/main/java/com/board/Test.java
+++ src/main/java/com/board/Test.java
@@ -2,7 +2,7 @@ package com.board;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Here is Team Board Branch");
+		System.out.println("Here is Team Board Branch!!");
 		
 		
 	}
