937d8b4da700b30f43fdac5b9ff22e13657a2a08
kimhewjung <ghlwnddl@naver.com>
2021-11-29 18:03:59 +0900

    123

--- src/main/java/com/board/Test2.java
+++ src/main/java/com/board/Test2.java
@@ -3,5 +3,7 @@ package com.board;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("second file");
+		System.out.println("±Ë»∏¡ﬂ");
+		System.out.println("±Ë»∏¡ﬂ");
 	}
 }
