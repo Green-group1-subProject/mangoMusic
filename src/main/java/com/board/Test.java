22276bbd96fa81276b10709261e8f9913676fd9b
DonghanKang <kdonghan2006@naver.com>
2021-11-29 17:29:42 +0900

    Start Team_User branch

--- src/main/java/com/board/Test.java
+++
@@ -1,9 +0,0 @@
-package com.board;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("from new_branch");
-		
-		
-	}
-}
---
+++ src/main/java/com/myPage/Test.java
@@ -0,0 +1,7 @@
+package com.myPage;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Here is Team_User Branch");
+	}
+}
